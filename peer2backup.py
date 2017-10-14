2ef504eb63194c3ab6d08a53a7121e45407e89be
Joel Michael <joelpmichael@gmail.com>
2017-10-14 13:13:44 +1030

    initial queuing work

--- peer2backup.py
+++ peer2backup.py
@@ -29,20 +29,77 @@ configdb_path = config.File(args.config)
 http_port = config.ConfigDb(configdb_path).Get('http.server.port',9336)
 num_worker_threads = config.ConfigDb(configdb_path).Get('worker.threads.count',multiprocessing.cpu_count())
 
+def _CreateWorkerQueueKey(self):
+    keychars = list('! #$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~')
+    new_key = []
+    for i in range(16):
+        new_key.append(random.choice(keychars))
+    key = ''.join(new_key)
+    found_in_queue=0
+    with worker_queue_lock:
+        if key in worker_queue:
+            found_in_queue=1
+    if found_in_queue:
+        return _CreateWorkerQueueKey() # self-recurse to generate a new key if one exists
+    else:
+        return key
+
 # set up work queue
-work_queue = queue.Queue()
+queue = queue.Queue()
+worker_queue_lock = threading.Lock()
+worker_queue = {}
+worker_status_lock = threading.Lock()
+worker_status = {}
 
 # set up worker threads
-def worker():
-    while True:
-        item = work_queue.get()
-        do_work(item)
-        work_queue.task_done()
+def _Worker():
+    # initial thread startup
+    with worker_status_lock:
+        worker_status[threading.current_thread().ident] = None
 
+    # thread main loop
+    while True:
+
+        # dequeue item
+        item = queue.get()
+            queue_item = []
+
+        # update worker status with item
+        with worker_status_lock:
+            worker_status[threading.current_thread().ident] = item
+
+        # grab item from queue
+        with worker_queue_lock:
+            queue_item = worker_queue[item]
+
+        # run item - item[0] is a bound method, item[1:] are its arguments
+        queue_item[0](*queue_item[1:])
+
+        # update worker status to idle
+        with worker_status_lock:
+            worker_status[threading.current_thread().ident] = None
+
+        # delete item from queue
+        with worker_queue_lock:
+            del worker_queue[item]
+
+        queue.task_done()
+
+# start threads
 for i in range(num_worker_threads):
-    t = threading.Thread(target=worker)
+    t = threading.Thread(target=_Worker)
+    t.daemon = True
     t.start()
-    threads.append(t)
+
+# set up scheduled task queuing thread
+def _Cron():
+    while True:
+        pass
+
+# start scheduled task queuing thread
+t = threading.Thread(target=_Cron)
+t.daemon = True
+t.start()
 
 import socket
 from socketserver import ThreadingMixIn
@@ -50,6 +107,13 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
     pass
+
+def status():
+    pass
+
+request_dictionary = {
+    '/status': status,
+}
 
 class RequestHandler(SimpleHTTPRequestHandler):
     # handler for HTTP server
